b0b8075c070ff171dc47b6cf3a5b485d624c6327
brayan <brayanjj.ch@gmail.com>
2026-04-28 09:39:11 -0400

    v5

--- frontend/src/components/DashboardCentro.tsx
+++ frontend/src/components/DashboardCentro.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CalendarCheck, Clock, QrCode, LogOut } from 'lucide-react';
 import './AdminDashboard.css';
--- frontend/src/components/DashboardCliente.tsx
+++ frontend/src/components/DashboardCliente.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import {useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { LogOut, Star, Download } from 'lucide-react';
 import './DashboardCliente.css'; 
